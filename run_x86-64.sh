c83c47ad68f76d54df4ffd48cc2caca75b138303
Ian Seyler <ian@seyler.me>
2016-12-01 20:06:37 -0500

    Better run script

--- run_x86-64.sh
+++ run_x86-64.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+# from http://unix.stackexchange.com/questions/9804/how-to-comment-multi-line-commands-in-shell-scripts
 
-qemu-system-x86_64 \
-	-machine q35 \
-	-cpu core2duo \
-	-curses \
-	-kernel ./boot.bin \
-	-s \
-	-net nic,model=e1000 \
-	-smp 2 \
+cmd=( qemu-system-x86_64
+	-machine q35
+	-cpu core2duo
+	-curses
+	-kernel ./boot.bin
+	-s
+	-net nic,model=e1000
+	-smp 2
 	-m 256
-#	-drive id=disk,file=bmfs.image,if=none,format=raw \
-#	-device ahci,id=ahci \
-#	-device ide-drive,drive=disk,bus=ahci.0 \
-#	-net dump,file=net.pcap \
-#	-serial file:serial.log \
+#	-drive id=disk,file=bmfs.image,if=none,format=raw
+#	-device ahci,id=ahci
+#	-device ide-drive,drive=disk,bus=ahci.0
+#	-net dump,file=net.pcap
+#	-serial file:serial.log
+)
+
+#execute the cmd string
+"${cmd[@]}"
