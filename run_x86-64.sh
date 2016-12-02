b53cfe5142bd8fac78cfabdf5bfcef05a5fbe05b
Ian Seyler <ian@seyler.me>
2016-12-01 19:37:50 -0500

    Fix run script

--- run_x86-64.sh
+++ run_x86-64.sh
@@ -4,14 +4,13 @@ qemu-system-x86_64 \
 	-machine q35 \
 	-cpu core2duo \
 	-curses \
-#	-drive id=disk,file=bmfs.image,if=none,format=raw \
-#	-device ahci,id=ahci \
-#	-device ide-drive,drive=disk,bus=ahci.0 \
-	-name "BareMetal"
 	-kernel ./boot.bin \
 	-s \
 	-net nic,model=e1000 \
-#	-net dump,file=net.pcap \
-#	-serial file:serial.log \
 	-smp 2 \
 	-m 256
+#	-drive id=disk,file=bmfs.image,if=none,format=raw \
+#	-device ahci,id=ahci \
+#	-device ide-drive,drive=disk,bus=ahci.0 \
+#	-net dump,file=net.pcap \
+#	-serial file:serial.log \
