00ba7e3202e9e836ec1ec2018f2d92896a13fe7f
Ian Seyler <ian.seyler@returninfinity.com>
2017-05-04 12:48:51 -0400

    Add error returns in disk calls

--- src/x86-64/syscalls/disk.asm
+++ src/x86-64/syscalls/disk.asm
@@ -12,20 +12,21 @@
 ; -----------------------------------------------------------------------------
 ; b_disk_read -- Read sectors from the disk
 ; IN:	RAX = Starting sector
-;	RCX = Number of sectors
+;	RCX = Number of sectors to read
 ;	RDX = Disk
 ;	RDI = Memory location to store data
-; OUT:	Nothing, all registers preserved
+; OUT:	RCX = Number of sectors read
+;	All other registers preserved
 b_disk_read:
 	push rdi
 	push rcx
 	push rax
 
 	cmp byte [os_DiskEnabled], 1	; Make sure that a disk is present
-	jne b_disk_read_done
+	jne b_disk_read_error
 
 	cmp rcx, 0
-	je b_disk_read_done		; Bail out if instructed to read nothing
+	je b_disk_read_error		; Bail out if instructed to read nothing
 	shl rax, 3			; Convert to 512B starting sector
 
 b_disk_read_loop:			; Read one sector at a time
@@ -41,26 +42,34 @@ b_disk_read_done:
 	pop rcx
 	pop rdi
 	ret
+
+b_disk_read_error:
+	pop rax
+	pop rcx
+	pop rdi
+	xor ecx, ecx
+	ret
 ; -----------------------------------------------------------------------------
 
 
 ; -----------------------------------------------------------------------------
 ; b_disk_write -- Write sectors to the disk
 ; IN:	RAX = Starting sector
-;	RCX = Number of sector
+;	RCX = Number of sectors to write
 ;	RDX = Disk
 ;	RSI = Memory location of data
-; OUT:	Nothing, all registers preserved
+; OUT:	RCX = Number of sectors written
+;	All other registers preserved
 b_disk_write:
 	push rsi
 	push rcx
 	push rax
 
 	cmp byte [os_DiskEnabled], 1	; Make sure that a disk is present
-	jne b_disk_write_done
+	jne b_disk_write_error
 
 	cmp rcx, 0
-	je b_disk_write_done		; Bail out if instructed to write nothing
+	je b_disk_write_error		; Bail out if instructed to write nothing
 	shl rax, 3			; Convert to 512B starting sector
 
 b_disk_write_loop:			; Write one sector at a time
@@ -75,6 +84,13 @@ b_disk_write_done:
 	pop rax
 	pop rcx
 	pop rsi
+	ret
+
+b_disk_write_error:
+	pop rax
+	pop rcx
+	pop rsi
+	xor ecx, ecx
 	ret
 ; -----------------------------------------------------------------------------
 
