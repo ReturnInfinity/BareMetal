dddf4d1e60bb407cf93e7c0e618beeed0258843c
Ian Seyler <ian.seyler@returninfinity.com>
2017-06-23 14:37:27 -0400

    Update build_x86-64.sh

--- build_x86-64.sh
+++ build_x86-64.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-export topdir=$(pwd)
-
-cd Pure64/
-./build.sh
-cp multiboot.sys ../
-cp pure64.sys ../
-cd ..
-
 cd src/x86-64/
 nasm kernel.asm -o ../../kernel.sys -l ../../kernel-debug.txt
 cd ../..
-
-cd BMFS/
-MAKEFLAGS="NO_FUSE=1 NO_UNIX_UTILS=1"
-make $MAKEFLAGS
-make $MAKEFLAGS install PREFIX=$topdir
-cd ..
-
-cd Alloy/
-./build.sh
-cp alloy.bin ../
-cd ..
-
-cat multiboot.sys pure64.sys kernel.sys alloy.bin > boot.bin
