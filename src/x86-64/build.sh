4b817d3d651bc0bc36ea9469b638213a586851ea
Ian Seyler <ian.seyler@returninfinity.com>
2017-11-10 11:44:55 -0500

    Update build.sh

--- src/x86-64/build.sh
+++ src/x86-64/build.sh
@@ -5,3 +5,4 @@ set -e
 nasm kernel.asm -o kernel.o -f elf64 -g -F dwarf
 ld kernel.o -o kernel.elf -T kernel.ld
 objcopy -O binary kernel.elf kernel.bin
+nasm -P nasm.ld kernel.asm -o ../../kernel.sys -l ../../kernel-debug.txt
