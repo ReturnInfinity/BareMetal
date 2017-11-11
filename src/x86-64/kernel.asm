0af2ad3c6a807c17d0990c48086cfd00d5dc5d3f
Ian Seyler <ian.seyler@returninfinity.com>
2017-11-10 22:12:23 -0500

    Update kernel.asm

--- src/x86-64/kernel.asm
+++ src/x86-64/kernel.asm
@@ -45,7 +45,7 @@ start:
 	mov rsi, 0x100000 + KERNELSIZE	; Payload starts right after the kernel
 	cmp qword [rsi], 0		; Is there a payload after the kernel?
 	je ap_clear			; If not, skip to ap_clear
-	mov rdi, 0x200000
+	mov rdi, 0x1E0000
 	mov rcx, 2048
 	rep movsq			; Copy 16384 bytes
 
