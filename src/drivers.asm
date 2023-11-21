8ece1fd3e933018861f7f2de688a33272194ed56
Ian <ian@ians-mac-pro.local>
2023-11-21 16:35:50 -0500

    Shorten drivers.asm list

--- src/drivers.asm
+++ src/drivers.asm
@@ -26,76 +26,82 @@ NIC_DeviceVendor_ID:	; The supported list of NICs
 ; The ID's are Device/Vendor
 
 ; Virtio
-dd 0x1AF4FFFF		; Driver marker
-dd 0x10001AF4
+;dw 0x1AF4		; Driver marker
+;dw 0x1AF4		; Vendor
+;dw 0x1000		; Device
+;dw 0x0000
 
 ; Intel 8254x/8257x Gigabit Ethernet
-dd 0x8254FFFF		; Driver marker
-dd 0x10008086		; 82542 (Fiber)
-dd 0x10018086		; 82543GC (Fiber)
-dd 0x10048086		; 82543GC (Copper)
-dd 0x10088086		; 82544EI (Copper)
-dd 0x10098086		; 82544EI (Fiber)
-dd 0x100A8086		; 82540EM
-dd 0x100C8086		; 82544GC (Copper)
-dd 0x100D8086		; 82544GC (LOM)
-dd 0x100E8086		; 82540EM
-dd 0x100F8086		; 82545EM (Copper)
-dd 0x10108086		; 82546EB (Copper)
-dd 0x10118086		; 82545EM (Fiber)
-dd 0x10128086		; 82546EB (Fiber)
-dd 0x10138086		; 82541EI
-dd 0x10148086		; 82541ER
-dd 0x10158086		; 82540EM (LOM)
-dd 0x10168086		; 82540EP (Mobile)
-dd 0x10178086		; 82540EP
-dd 0x10188086		; 82541EI
-dd 0x10198086		; 82547EI
-dd 0x101a8086		; 82547EI (Mobile)
-dd 0x101d8086		; 82546EB
-dd 0x101e8086		; 82540EP (Mobile)
-dd 0x10268086		; 82545GM
-dd 0x10278086		; 82545GM
-dd 0x10288086		; 82545GM
-dd 0x105b8086		; 82546GB (Copper)
-dd 0x105e8086		; 82571EB/82571GB
-dd 0x105f8086		; 82571EB
-dd 0x10608086		; 82571EB
-dd 0x10758086		; 82547GI
-dd 0x10768086		; 82541GI
-dd 0x10778086		; 82541GI
-dd 0x10788086		; 82541ER
-dd 0x10798086		; 82546GB
-dd 0x107a8086		; 82546GB
-dd 0x107b8086		; 82546GB
-dd 0x107c8086		; 82541PI
-dd 0x107d8086		; 82572EI (Copper)
-dd 0x107e8086		; 82572EI (Fiber)
-dd 0x107f8086		; 82572EI
-dd 0x108b8086		; 82573V (Copper)
-dd 0x108c8086		; 82573E (Copper)
-dd 0x109a8086		; 82573L
-dd 0x10a48086		; 82571EB
-dd 0x10a58086		; 82571EB (Fiber)
-dd 0x10b58086		; 82546GB (Copper)
-dd 0x10b98086		; 82572EI (Copper)
-dd 0x10bc8086		; 82571EB/82571GB (Copper)
-dd 0x10c98086		; 82576
-dd 0x10d38086		; 82574L
-dd 0x10d68086		; 82575GB
-dd 0x10e28086		; 82575GB
-dd 0x10e68086		; 82576
-dd 0x10e78086		; 82576
-dd 0x10e88086		; 82576
-dd 0x10ea8086		; 82577LM
-dd 0x10eb8086		; 82577LC
-dd 0x10ef8086		; 82578DM
-dd 0x10f08086		; 82578DC
-dd 0x10f68086		; 82574L
-dd 0x11078086		; 82544EI
-dd 0x11128086		; 82544GC
+dw 0x8254		; Driver marker
+dw 0x8086		; Vendor
+dw 0x1000		; 82542 (Fiber)
+dw 0x1001		; 82543GC (Fiber)
+dw 0x1004		; 82543GC (Copper)
+dw 0x1008		; 82544EI (Copper)
+dw 0x1009		; 82544EI (Fiber)
+dw 0x100A		; 82540EM
+dw 0x100C		; 82544GC (Copper)
+dw 0x100D		; 82544GC (LOM)
+dw 0x100E		; 82540EM
+dw 0x100F		; 82545EM (Copper)
+dw 0x1010		; 82546EB (Copper)
+dw 0x1011		; 82545EM (Fiber)
+dw 0x1012		; 82546EB (Fiber)
+dw 0x1013		; 82541EI
+dw 0x1014		; 82541ER
+dw 0x1015		; 82540EM (LOM)
+dw 0x1016		; 82540EP (Mobile)
+dw 0x1017		; 82540EP
+dw 0x1018		; 82541EI
+dw 0x1019		; 82547EI
+dw 0x101a		; 82547EI (Mobile)
+dw 0x101d		; 82546EB
+dw 0x101e		; 82540EP (Mobile)
+dw 0x1026		; 82545GM
+dw 0x1027		; 82545GM
+dw 0x1028		; 82545GM
+dw 0x105b		; 82546GB (Copper)
+dw 0x105e		; 82571EB/82571GB
+dw 0x105f		; 82571EB
+dw 0x1060		; 82571EB
+dw 0x1075		; 82547GI
+dw 0x1076		; 82541GI
+dw 0x1077		; 82541GI
+dw 0x1078		; 82541ER
+dw 0x1079		; 82546GB
+dw 0x107a		; 82546GB
+dw 0x107b		; 82546GB
+dw 0x107c		; 82541PI
+dw 0x107d		; 82572EI (Copper)
+dw 0x107e		; 82572EI (Fiber)
+dw 0x107f		; 82572EI
+dw 0x108b		; 82573V (Copper)
+dw 0x108c		; 82573E (Copper)
+dw 0x109a		; 82573L
+dw 0x10a4		; 82571EB
+dw 0x10a5		; 82571EB (Fiber)
+dw 0x10b5		; 82546GB (Copper)
+dw 0x10b9		; 82572EI (Copper)
+dw 0x10bc		; 82571EB/82571GB (Copper)
+dw 0x10c9		; 82576
+dw 0x10d3		; 82574L
+dw 0x10d6		; 82575GB
+dw 0x10e2		; 82575GB
+dw 0x10e6		; 82576
+dw 0x10e7		; 82576
+dw 0x10e8		; 82576
+dw 0x10ea		; 82577LM
+dw 0x10eb		; 82577LC
+dw 0x10ef		; 82578DM
+dw 0x10f0		; 82578DC
+dw 0x10f6		; 82574L
+dw 0x1107		; 82544EI
+dw 0x1112		; 82544GC
+dw 0x0000
 
-dq 0x0000000000000000	; End of list
+dw 0x0000		; End of list
+dw 0x0000
+dw 0x0000
 
 
 ; =============================================================================
