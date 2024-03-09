50743aa2765c9a5a505c8afa1afff61debc981be
Ian <ian@ians-mac-pro.local>
2024-03-09 15:10:21 -0500

    Start work on os_pcie_convert

--- src/drivers/pcie.asm
+++ src/drivers/pcie.asm
@@ -15,25 +15,12 @@
 ; OUT:	EAX = Register value that was read
 ;	All other registers preserved
 os_pcie_read:
-	push rsi
 	push rdx
-	; ror rdx, 32			; Move segment to DX
-	; Load RSI with the base memory of the selected PCIe Segment
-	mov rsi, 0xb0000000		; QEMU
 
-	push rdx			; Save RDX for the register
-	and edx, 0xFFFF0000		; Isolate the device/function
-	shr edx, 4			; Quick divide by 16
-	add rsi, rdx			; RSI now points to the start of the 4KB register memory
-	pop rdx				; Low 10 bits of RDX is the register
-	and edx, 0x000003FF		; Only keep the low 10 bits
-	shl edx, 2			; Quick multiply by 4
-	add rsi, rdx			; Add offset for the register
-
-	lodsd				; Load a 32-bit PCIe register
+	call os_pcie_convert		; Convert RDX to memory address
+	mov eax, [rdx]			; Load register value
 
 	pop rdx
-	pop rsi
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -44,25 +31,41 @@ os_pcie_read:
 ;	EAX = Register value to be written
 ; OUT:	Nothing, all registers preserved
 os_pcie_write:
-	push rdi
 	push rdx
-	; ror rdx, 32			; Move segment to DX
-	; Load RDI with the base memory of the selected PCI Segment
-	mov rdi, 0xb0000000		; QEMU
 
+	call os_pcie_convert		; Convert RDX to memory address
+	mov [rdx], eax			; Store register value
+
+	pop rdx
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; os_pcie_convert -- Convert RDX to the memory address of the register
+;  IN:	RDX = Register to read/write
+;	RDX = Memory address of register
+; OUT:	Nothing, all registers preserved
+os_pcie_convert:
+	push rsi
+	push rax
+
+	; Check the submitted Segment Group against known ones
+	mov rsi, [0x5400]
+	
+	; Add offset to the correct device/function/register
 	push rdx			; Save RDX for the register
 	and edx, 0xFFFF0000		; Isolate the device/function
 	shr edx, 4			; Quick divide by 16
-	add rdi, rdx			; RSI now points to the start of the 4KB register memory
+	add rsi, rdx			; RSI now points to the start of the 4KB register memory
 	pop rdx				; Low 10 bits of RDX is the register
 	and edx, 0x000003FF		; Only keep the low 10 bits
 	shl edx, 2			; Quick multiply by 4
-	add rdi, rdx			; Add offset for the register
+	add rsi, rdx			; Add offset for the register
+	mov rdx, rsi			; Store final memory address in RDX
 
-	stosd				; Store a 32-bit PCIe register
-
-	pop rdx
-	pop rdi
+	pop rax
+	pop rsi
 	ret
 ; -----------------------------------------------------------------------------
 
