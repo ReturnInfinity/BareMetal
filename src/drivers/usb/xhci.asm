3fadb9336250b54024167179bf41c87b6551853d
Ian Seyler <ian@ians-mac-mini.local>
2025-01-20 15:49:54 -0500

    XHCI - Add port resets

--- src/drivers/usb/xhci.asm
+++ src/drivers/usb/xhci.asm
@@ -35,9 +35,10 @@ xhci_init:
 	bts rax, 4			; Set PCD to disable caching
 	mov [rdi], rax
 
-	; Gather CAPLENGTH and check for a valid version number
+	; Gather CAPLENGTH and HCIVERSION
 	mov eax, [rsi+XHCI_CAPLENGTH]	; Read 4 bytes starting at CAPLENGTH
 	mov [xhci_caplen], al		; Save the CAPLENGTH offset
+	; Check for a valid version number
 	shr eax, 16			; 16-bit version is in bits 31:16, shift to 15:0
 	cmp ax, 0x0100			; Verify it is at least v1.0
 	jl xhci_init_error
@@ -79,12 +80,31 @@ xhci_init_reset:
 	mov [rdi+XHCI_CRCR], rax	; Set the Command Ring Control Register
 	mov eax, [rsi+XHCI_HCSPARAMS1]	; Gather MaxSlots (bits 7:0)
 	and eax, 0x000000FF		; Keep bits 7:0
+	mov byte [xhci_maxslots], al
 	mov [rdi+XHCI_CONFIG], eax
 	mov eax, 1
 	mov [rdi+XHCI_DNCTRL], eax
 	mov eax, 0x0D			; Set bits 0 (RS), 2 (INTE), and 3 (HSEE)
 	mov [rdi+XHCI_USBCMD], eax
-	
+
+	; Check the available ports and reset them
+	xor ecx, ecx			; Slot counter
+	xor edx, edx			; Max slots
+	mov dl, byte [xhci_maxslots]
+xhci_check_next:
+	mov ebx, 0x400			; Offset to start of Port Registers
+	shl ecx, 5			; Quick multiply by 16
+	add ebx, ecx			; Add offset to EBX
+	shr ecx, 5			; Quick divide by 16
+	mov eax, [rdi+rbx]		; Load PORTSC
+	bt eax, 0			; Current Connect Status
+	jnc xhci_reset_skip
+	bts eax, 4			; Port Reset
+	mov [rdi+rbx], eax
+xhci_reset_skip:
+	inc ecx
+	cmp ecx, edx
+	jne xhci_check_next	
 	jmp xhci_init_done
 
 xhci_init_error:
@@ -96,6 +116,7 @@ xhci_init_done:
 	ret
 
 xhci_caplen:	db 0
+xhci_maxslots:	db 0
 ; -----------------------------------------------------------------------------
 
 
@@ -113,7 +134,7 @@ XHCI_RTSOFF	equ 0x18	; 4-byte Runtime Registers Space Offset
 XHCI_HCCPARMS2	equ 0x1C	; 4-byte Capability Parameters 2 (XHCI v1.1+)
 XHCI_VTIOSOFF	equ 0x20	; 4-byte VTIO Register Space Offset (XHCI v1.2+)
 
-; Host Controller Operational Registers (Starts at XHCI_Base + value in CAPLENGTH)
+; Host Controller Operational Registers (Starts at XHCI_Base + CAPLENGTH)
 XHCI_USBCMD	equ 0x00	; 4-byte USB Command Register
 XHCI_USBSTS	equ 0x04	; 4-byte USB Status Register
 XHCI_PAGESIZE	equ 0x08	; 4-byte Page Size Register (Read-Only)
@@ -122,7 +143,7 @@ XHCI_CRCR	equ 0x18	; 8-byte Command Ring Control Register
 XHCI_DCBAPP	equ 0x30	; 8-byte Device Context Base Address Array Pointer Register
 XHCI_CONFIG	equ 0x38	; 4-byte Configure Register
 
-; Host Controller USB Port Register Set
+; Host Controller USB Port Register Set (Starts at XHCI_Base + CAPLENGTH + 0x0400 - 16 bytes per port)
 XHCI_PORTSC	equ 0x00	; 4-byte Port Status and Control Register
 XHCI_PORTPMSC	equ 0x04	; 4-byte Port PM Status and Control Register
 XHCI_PORTLI	equ 0x08	; 4-byte Port Link Info Register (Read-Only)
