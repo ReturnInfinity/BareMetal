6479279f8a40e220bb150e194a1bedae89ac8973
Ian Seyler <ian@ians-macbook-air.local>
2025-05-27 15:56:02 -0400

    Fix for aligned video memory - Also remove use of 64-bit registers where not needed

--- src/drivers/lfb/lfb.asm
+++ src/drivers/lfb/lfb.asm
@@ -85,7 +85,7 @@ render_done:
 	mov al, [font_height]
 	mov cl, [font_width]
 	mul ecx				; EDX:EAX := EAX * ECX
-	shl rax, 2			; Quick multiply by 4
+	shl eax, 2			; Quick multiply by 4
 	mov [lfb_glyph_bytes], eax
 
 	; Pixels per row = font_h * [os_screen_x] * 4 * [Screen_Cursor_Row]
@@ -96,18 +96,18 @@ render_done:
 	mov ax, [os_screen_x]
 	mov cx, font_h			; Font height
 	mul ecx				; EDX:EAX := EAX * ECX
-	shl rax, 2			; Quick multiply by 4
+	shl eax, 2			; Quick multiply by 4
 	mov [lfb_glyph_bytes_per_row], eax
 
 	xor eax, eax
 	mov ax, font_w
-	shl rax, 2
+	shl eax, 2			; Quick multiply by 4
 	mov [lfb_glyph_bytes_per_col], eax
 
 	xor eax, eax
-	mov ax, [os_screen_x]
-	sub ax, font_w
-	shl eax, 2
+	mov ax, [os_screen_ppsl]	; Use Pixels Per Scan Line in case system aligns video memory
+	sub ax, font_w			; Subtract the width of a single glyph
+	shl eax, 2			; Quick multiply by 4
 	mov [lfb_glyph_next_line], rax
 
 	mov rax, [os_screen_lfb]
