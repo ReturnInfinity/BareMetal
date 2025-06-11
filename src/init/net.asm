d50ca76f68b63e6b972775975dfdbdff79a5b557
Ian Seyler <ian@ians-macbook-air.local>
2025-06-11 17:43:51 -0400

    `init_net` can now find multiple network devices - Currently stops after 2 have been initialized

--- src/init/net.asm
+++ src/init/net.asm
@@ -11,8 +11,7 @@
 init_net:
 	; Check Bus Table for a Ethernet device
 	mov rsi, bus_table		; Load Bus Table address to RSI
-	sub rsi, 16
-	add rsi, 8			; Add offset to Class Code
+	sub rsi, 8			; Subtract offset for Class Code
 init_net_check_bus:
 	add rsi, 16			; Increment to next record in memory
 	mov ax, [rsi]			; Load Class Code / Subclass Code
@@ -29,6 +28,8 @@ init_net_probe_find_driver:
 	mov edx, [rsi]			; Load value for os_bus_read/write
 	mov r8d, [rsi+4]		; Save the Device ID / Vendor ID in R8D
 	rol r8d, 16			; Swap the Device ID / Vendor ID
+	add rsi, 8			; Move RSI back to Class Code
+	push rsi
 	mov rsi, NIC_DeviceVendor_ID
 init_net_probe_find_next_driver:
 	lodsw				; Load a driver ID
@@ -44,8 +45,8 @@ init_net_probe_find_next_device:
 	cmp eax, r8d
 	je init_net_probe_found		; If Carry is clear then we found a supported NIC
 	jmp init_net_probe_find_next_device	; Check the next device
-
 init_net_probe_found:
+	pop rsi
 	cmp bx, 0x1AF4
 	je init_net_probe_found_virtio
 	cmp bx, 0x8254
@@ -79,12 +80,17 @@ init_net_probe_found_r8169:
 	jmp init_net_probe_found_finish
 
 init_net_probe_found_finish:
-	add byte [os_net_icount], 1
 	mov byte [os_NetEnabled], 1	; A supported NIC was found. Signal to the OS that networking is enabled
 	add r9, 15			; Add offset to driver enabled byte
 	mov byte [r9], 1		; Mark device as having a driver
+	add byte [os_net_icount], 1
+	cmp byte [os_net_icount], 2	; Have 2 NIC's been activated?
+	je init_net_end			; If so, bail out as 2 is the max at the moment
+	jmp init_net_check_bus		; Check for another network device
 
 init_net_probe_not_found:
+
+init_net_end:
 	; Output block to screen (4/4)
 	mov ebx, 6
 	call os_debug_block
